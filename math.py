7378ad60c591fdcb58936fbe886613a25f842b6c
pankajduthde <pankajduthade71@gmail.com>
2025-07-12 22:42:31 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,30 @@
+num1 = int(input("Enter the first number :"))
+num2 = int(input("Enter the second number :"))
+int(num1+num2)
+print("addition",num1+num2)
+
+num3 =int(input("Enter the third number :"))
+num4 = int(input("Enter the fourth number :"))
+int(num3-num4)
+print("subtraction",num3-num4)
+
+num5 = int(input("Enter the fifth number :"))
+num6 = int(input("Enter the sixth number :"))
+int(num5*num6)
+print("multiplication",num5*num6)
+
+num7 = int(input("Enter the seventh number :"))
+num8 = int(input("Enter the eighth number :"))
+int(num7/num8)
+print("division",num7/num8)
+
+# Step 1: Take input from the user
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Step 2: Concatenate first and last name
+full_name = first_name + " " + last_name
+
+# Step 3: Print a personalized greeting
+print("Hello, " + full_name + "! Welcome to the python program.")
+
